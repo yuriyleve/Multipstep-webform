60fc1111bacf493a301d78c9090e2790bb6c8a14
Yuriy Leve <yuriyleve@gmail.com>
2023-10-19 13:12:39 -0300

    when i enter a value in the formfield then next step is also activated

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -13,19 +13,19 @@ const store = useContainerStore()
                 </router-link>
             </li>
             <li>
-                <router-link to="/ct/info/plan" v-if="store.container.name">
+                <router-link to="/ct/info/plan" v-if="store.container.page > 1">
                     <span :class="{ 'font-bold': store.container.page === 2 }">Select plan</span>
                 </router-link>
                 <span v-else class="text-gray-300">Select plan</span>
             </li>
             <li>
-                <router-link to="/ct/info/domain" v-if="store.container.plan">
+                <router-link to="/ct/info/domain" v-if="store.container.page > 2">
                     <span :class="{ 'font-bold': store.container.page === 3 }">Custom domain</span>
                 </router-link>
                 <span v-else class="text-gray-300">Custom domain</span>
             </li>
             <li>
-                <router-link to="/ct/info/dns" v-if="store.container.domain">
+                <router-link to="/ct/info/dns" v-if="store.container.page > 3">
                     <span :class="{ 'font-bold': store.container.page === 4 }">Review</span>
                 </router-link>
                 <span v-else class="text-gray-300">Finish</span>
